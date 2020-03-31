39d42e8bf2544c8c1b59ef57372b38b33ef23b06
shubhamr238 <shubhamr238@gmail.com>
2020-03-31 20:27:59 +0530

    add deleteProduct

--- controller/productController.js
+++ controller/productController.js
@@ -44,3 +44,15 @@ module.exports.getProduct = function(req,res){
       }});
     });
 };
+
+module.exports.deleteProduct = function(req, res) {
+
+    Product.findByIdAndRemove(req.params.id, function(err) {
+      if (err){
+        console.error("Error in Deleting Product",err);
+        return res.redirect("/");
+      }else{
+        return res.json({data:{message:"product deleted"}});
+      }
+    });
+};
--- routes/products.js
+++ routes/products.js
@@ -7,5 +7,6 @@ const productController = require("../controller/productController");
 router.post("/create", productController.addProduct);
 router.post("/:id/update_quantity", productController.updateProduct);
 router.get("/", productController.getProduct);
+router.post("/:id", productController.deleteProduct);
 
 module.exports = router;
